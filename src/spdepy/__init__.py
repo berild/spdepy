03307d9fa94ca6ab9dd1637115d845b3e43b8c2e
Martin Outzen Berild <martinob@Martins-MacBook-Pro.local>
2023-11-10 16:38:38 +0100

    change to new class names and add constructor

--- src/spdepy/__init__.py
+++ src/spdepy/__init__.py
@@ -2,6 +2,15 @@
 from importlib.metadata import version
 __version__ = version("spdepy")
 
-from spdepy.spde import SPDE
-from spdepy.model import *
-from spdepy.grid import *
+from .diffusion import diffusion
+from .advection import advection
+from .model import Model 
+from .grid import reg_grid
+
+def model(grid = None, diff = None, adv = None) -> Model:
+    grid = reg_grid() if grid is None else grid
+    if isinstance(diff,str):
+        diff = diffusion(grid,par = None,bc = 3)
+    if isinstance(adv,str):
+        adv = advection(grid,par = None,bc = 3)
+    return(Model(grid = grid, diffusion=diff , advection = adv))
