ea82f7383f2cad398b12c2c4e1ff247bbd5f167a
Martin Outzen Berild <martinob@Martins-MacBook-Pro.local>
2023-11-10 16:32:57 +0100

    add grid subpackage

--- src/spdepy/grid/__init__.py
+++ src/spdepy/grid/__init__.py
@@ -1,4 +1,22 @@
-# 2d
-# 3d
-# 2d + time
-# 3d + time
+
+
+def reg_grid(x,y,z= None, t = None,extend = None):
+    if z is None:
+        if t is None:
+            from .spatial2D_regular_mesh import Grid
+            grid = Grid()
+            grid.setGrid(x=x,y=y,extend = extend)
+            return(grid)
+        else:
+            from .spatial2D_regular_mesh import Grid
+            grid = Grid()
+            grid.setGrid(x=x,y=y,extend = extend)
+            return(grid)
+    else:
+        if t is None:
+            from .spatial3D_regular_mesh import Grid
+            grid = Grid()
+            grid.setGrid(x=x,y=y,z = z,extend = extend)
+            return(grid)
+        else:
+            assert False, "4D grids are not supported yet"
