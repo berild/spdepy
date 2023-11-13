57e8fa347dd048234eeaceebc2aa3c1af855e9b6
Martin Outzen Berild <martinob@Martins-MacBook-Pro.local>
2023-11-13 09:29:46 +0100

    change subpackage names

--- src/spdepy/__init__.py
+++ src/spdepy/__init__.py
@@ -2,15 +2,15 @@
 from importlib.metadata import version
 __version__ = version("spdepy")
 
-from .diffusion import diffusion
-from .advection import advection
+from spdepy.diffusions import diffusion
+from spdepy.advections import advection
 from .model import Model 
-from .grid import reg_grid
+from spdepy.grids import grid
 
-def model(grid = None, diff = None, adv = None) -> Model:
-    grid = reg_grid() if grid is None else grid
+def model(mesh = None, diff = None, adv = None) -> Model:
+    mesh = grid() if grid is None else grid
     if isinstance(diff,str):
         diff = diffusion(grid,par = None,bc = 3)
     if isinstance(adv,str):
         adv = advection(grid,par = None,bc = 3)
-    return(Model(grid = grid, diffusion=diff , advection = adv))
+    return(Model(mesh = mesh, diffusion=diff , advection = adv))
