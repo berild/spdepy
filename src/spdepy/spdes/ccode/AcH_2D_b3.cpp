d7f513c77e1f857176d2a837ccfda9466ed140b5
Martin Outzen Berild <martinob@dhcp-10-24-17-152.wlan.ntnu.no>
2024-02-01 18:24:06 +0100

    add comment about usage of ccode

--- src/spdepy/spdes/ccode/AcH_2D_b3.cpp
+++ src/spdepy/spdes/ccode/AcH_2D_b3.cpp
@@ -3,6 +3,7 @@
 
 class AH
 {
+    // Construction of diffusion matrix of the solution of 2D Advection-Diffsion equation
     // Dirichiet boundary conditions with value equal zero one the boundaries
     public:
         AH(int numX, int numY, double H[2][2],double hx,double hy);
