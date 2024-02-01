bd899fb46b3e01a52ef86d9bd4ba618ce75c96d5
Martin Outzen Berild <martinob@dhcp-10-24-17-152.wlan.ntnu.no>
2024-02-01 19:21:02 +0100

    add doctring

--- src/spdepy/grids/__init__.py
+++ src/spdepy/grids/__init__.py
@@ -1,6 +1,33 @@
 
 
 def grid(x,y,z= None, t = None,extend = None):
+    """Constructor for Grid class. 
+    Supported domains are Spatial 2D, Spatial 3D or Spatio-temporal 2D are supported.
+    These classes also constructs the basis spline functions for the spatially varying parameters within the SPDE.
+    See Grid classes for more information.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        x coordinates of grid cell centers in a regular grid.
+        Like longitude, or meters in some coordinate system
+    y : np.ndarray
+        y coordinates of grid cell centers in a regular grid.
+        Like latitude, or meters in some coordinate system
+    z : np.ndarray, optional
+        z coordiantes of grid cell centers in a regular grid of 3D spatial domain
+        Like depth or altitude in some coordinate system. If unspecified 2D spatial domain will be used. 
+    t : np.ndarrray, optional
+        time coordinates. If unspecified, spatial model will be used or error will be raised.
+    extend : int, optional
+        Optional extension of grid in all direction by extend grid cells. 
+        The same step length is used for this extension. If unspecified, no extension is used.
+    Returns
+    ----------
+    Grid
+        Grid object of the specified domain.
+        Spatial 2D, Spatial 3D or Spatio-temporal 2D are supported.
+    """
     if z is None:
         if t is None:
             from .spatial2D_regular_mesh import Grid
