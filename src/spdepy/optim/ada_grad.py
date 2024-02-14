64dfd704e7a1ce62cfc0133e1350f738ca27c746
Martin Outzen Berild <martinob@Martins-MacBook-Pro.local>
2024-02-14 16:47:15 +0100

    add ada_grad to /optim

--- src/spdepy/optim/ada_grad.py
+++ src/spdepy/optim/ada_grad.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class Adagrad:
-    def __init__(self ) -> None:
+    def __init__(self , epsilon = 1e-8) -> None:
         self.g2 = None
-        self.dx = 0
+        self.epsilon = epsilon
     
     def __call__(self, x, f, gf, lr,epsilon=1e-8) -> np.ndarray:
         if self.g2 is None:
@@ -13,3 +13,15 @@ class Adagrad:
         self.dx = -lr*gf/(np.sqrt(self.g2)+ epsilon)
         return(x + self.dx)
     
+    def settings(self, **kwargs) -> None:
+        if kwargs.get("epsilon") is not None:
+            self.epsilon = kwargs.get("epsilon")
+        self.reset()
+        pass
+    
+    def reset(self) -> None:
+        self.g2 = None
+    
+    def printInit(self) -> None:
+        print(f"AdaGrad optimizer with epsilon: {self.epsilon}")
+        print("--------------------------------------------------------------")
