f914e483089f0ede31f4a32ed5c8c2e10b223439
Martin Outzen Berild <martinob@Martins-MacBook-Pro.local>
2024-02-19 11:10:22 +0100

    add non spatially varying whittle matern spde solving

--- src/spdepy/spdes/whittle_matern_anisotropic2D.py
+++ src/spdepy/spdes/whittle_matern_anisotropic2D.py
@@ -21,7 +21,7 @@ class WhittleMaternAnisotropic2D:
         self.AHnew = None
         self.Awnew = None
         if par is None: 
-            par = np.hstack([[-1]*9,[-1]*9, [0.1]*9, [0.1]*9,1],dtype = "float64")
+            par = np.hstack([-1,-1,0.1,0.1,0],dtype = "float64")
             self.setPars(par)
         else:
             self.setQ(par = par)
@@ -31,17 +31,16 @@ class WhittleMaternAnisotropic2D:
     
     def setPars(self,par) -> None:
         par = np.array(par,dtype="float64")
-        self.kappa = par[0:9]
-        self.gamma = par[9:18]
-        self.vx = par[18:27]
-        self.vy = par[27:36]
-        self.tau = par[36]
+        self.kappa = par[0]
+        self.gamma = par[1]
+        self.vx = par[2]
+        self.vy = par[3]
+        self.tau = par[4]
         self.sigma = np.log(np.sqrt(1/np.exp(self.tau)))
 
     def initFit(self,data, **kwargs):
-        #mod4: kappa(0:9), gamma(9:18), vx(18:27), vy(27:36), sigma(36)
         assert data.shape[0] <= self.grid.Ns
-        par = np.hstack([[-1]*9,[-1]*9, [0.1]*9, [0.1]*9,1],dtype = "float64")
+        par = np.hstack([-1,-1,0.1,0.1,0],dtype = "float64")
         self.data = data
         if self.data.ndim == 2:
             self.r = self.data.shape[1]
@@ -50,112 +49,102 @@ class WhittleMaternAnisotropic2D:
         self.S = self.grid.getS()
         return(par)
 
-    def sample(self,n = 1,simple = False):
-        z = np.random.normal(size = self.grid.Ns*n).reshape(self.grid.Ns,n)
-        data = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False))
-        if not simple:
-            data += z*np.exp(self.sigma)
-        return(data)
 
-
-    def setQ(self,par = None,S = None, simple = False):
+    def setQ(self,par = None,S = None):
         if par is None:
-            assert(self.kappa is not None and self.vx is not None and self.vy is not None and self.gamma is not None and self.sigma is not None)
             par = self.getPars()
         else:
             self.setPars(par)
-        if S is not None:
-            self.S = S
-        Hs = self.getH()
-        Dk =  sparse.diags(np.exp(self.grid.evalB(par = self.kappa))) 
-        A_mat = self.grid.Dv@Dk - self.Ah(Hs)
-        self.Q = A_mat.T@self.grid.iDv@A_mat
+        self.Q, self.Q_fac = self.makeQ(par = par, grad = False)
         self.S = self.grid.getS()
-        if not simple:
-            self.Q_fac = cholesky(self.Q)
 
-    def getH(self,gamma=None,vx = None,vy = None,d=None,grad = False):
-        if vx is None and vy is None and gamma is None:
-            gamma = self.gamma
-            vx = self.vx
-            vy = self.vy
-        if not grad:
-            pg = np.exp(self.grid.evalBH(par = gamma))
-            vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy)],axis=2)
-            H = (np.eye(2)*(np.stack([pg,pg],axis=2))[:,:,:,np.newaxis]) + vv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:]
-            return(H)
-        else:
-            dpar = np.zeros(9)
-            dpar[d] = 1
-
-            vv = np.stack([self.grid.evalBH(par = vx),self.grid.evalBH(par = vy)],axis=2)
-            pg = np.exp(self.grid.evalBH(par = gamma))
-            H_gamma = np.eye(2)*(np.stack([self.grid.bsH[:,:,d]*pg,self.grid.bsH[:,:,d]*pg],axis=2)[:,:,:,np.newaxis])
-
-            dv = np.stack([self.grid.evalBH(par = dpar),self.grid.evalBH(par = np.zeros(9))],axis = 2)
-            H_vx = vv[:,:,:,np.newaxis]*dv[:,:,np.newaxis,:]  + dv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:]
-
-            dv = np.stack([self.grid.evalBH(par = np.zeros(9)),self.grid.evalBH(par = dpar)],axis = 2)
-            H_vy = vv[:,:,:,np.newaxis]*dv[:,:,np.newaxis,:]  + dv[:,:,:,np.newaxis]*vv[:,:,np.newaxis,:] 
-            return((H_gamma,H_vx,H_vy))
-
-    def print(self,par):
-        return("| \u03BA = %2.2f"%(np.exp(par[0:9]).mean()) +  ", \u03B3 = %2.2f"%(np.exp(par[9:18]).mean()) + ", vx = %2.2f"%(par[18:27].mean()) + ", vy = %2.2f"%(par[27:36].mean()) +  ",\u03C4 = %2.2f"%(np.exp(par[36])))
-    
-    def logLike(self, par, nh1 = 100,grad = True):
-        #mod4: kappa(0:9), gamma(9:18), vx(18:27), vy(27:36), sigma(36)
-        data  = self.data
-        Hs = self.getH(gamma = par[9:18],vx = par[18:27], vy = par[27:36]) 
-        lkappa = self.grid.evalB(par = par[0:9])
-        Dk =  sparse.diags(np.exp(lkappa)) 
+    def makeQ(self, par, grad = True):
+        assert self.Q0 is not None
+        Ns = self.grid.Ns
         Dv = self.grid.Dv
         iDv = self.grid.iDv
-        A_mat = Dv@Dk - self.Ah(Hs)
-        Q = A_mat.transpose()@iDv@A_mat
-        Q_c = Q + self.S.transpose()@self.S*np.exp(par[36])
+        # parameters
+        kappa = np.exp(par[0])
+        gamma = np.exp(par[1])
+        vv = par[2:4]
+        # components
+        Hs = gamma*np.eye(2) + vv[:,np.newaxis]*vv[np.newaxis,:]
+        Dk =  kappa*sparse.eye(Ns) 
+        A =  Dv@Dk - self.Ah(Hs)
+        Q = A.transpose()@iDv@A
+        # gradient
         Q_fac = cholesky(Q)
-        Q_c_fac= cholesky(Q_c)
-        if (Q_fac == -1) or (Q_c_fac == -1):
-            if grad:
-                return((self.like,self.jac))
-            else:
-                return(self.like)
-        mu_c = Q_c_fac.solve_A(self.S.transpose()@data*np.exp(par[36]))
-        if self.r == 1:
-            data = data.reshape(-1,1)
-            mu_c = mu_c.reshape(-1,1)
-        like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*par[36]/2 - 1/2*Q_c_fac.logdet()*self.r - 1/2*(mu_c*(Q@mu_c)).sum() - np.exp(par[36])/2*((data-self.S@mu_c)**2).sum()
         if grad:
-            vtmp = (2*np.random.randint(1,3,self.grid.Ns*nh1)-3).reshape(self.grid.Ns,nh1)
+            dQ = []
+            # log kappa 2
+            dA = (Dv@Dk).tocsc()
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            # log gamma
+            dHs = gamma*np.eye(2) 
+            dA = - self.Ah(dHs)
+            A =  Dv@Dk - self.Ah(Hs)
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            # vx 
+            dvv = np.array([1,0],dtype = "float64") 
+            dHs = dvv[:,np.newaxis]*vv[np.newaxis,:] + vv[:,np.newaxis]*dvv[np.newaxis,:] 
+            dA = - self.Ah(dHs)
+            A =  Dv@Dk - self.Ah(Hs)
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            # vy
+            dvv = np.array([0,1],dtype = "float64")
+            dHs = dvv[:,np.newaxis]*vv[np.newaxis,:] + vv[:,np.newaxis]*dvv[np.newaxis,:] 
+            dA = - self.Ah(dHs)
+            A =  Dv@Dk - self.Ah(Hs)
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            return(Q, Q_fac, dQ)
+        else:
+            return(Q, Q_fac)
+         
+    def print(self,par):
+        return("| \u03BA = %2.2f"%(np.exp(par[0])) +  ", \u03B3 = %2.2f"%(np.exp(par[1])) + ", vx = %2.2f"%(par[2]) + ", vy = %2.2f"%(par[3]) +  ",\u03C4 = %2.2f"%(np.exp(par[4])))
+    
+    def logLike(self, par, nh1 = 100, grad = True):
+        if grad:
+            data  = self.data
+            tau = np.exp(par[-1])
+            Q, Q_fac, dQ = self.makeQ(par = par, grad = True)
+            Q_c = Q + self.S.T@self.S*tau
+            Q_c_fac = cholesky(Q_c)
+            mu_c = Q_c_fac.solve_A(self.S.T@data*tau)
+            if self.r == 1:
+                data = data.reshape(-1,1)
+                mu_c = mu_c.reshape(-1,1)
+            like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*np.log(tau)/2 - 1/2*Q_c_fac.logdet()*self.r - 1/2*(mu_c*(Q@mu_c)).sum() - tau/2*((data-self.S@mu_c)**2).sum()
+            
+            vtmp = (2*np.random.randint(1,3,self.grid.n*nh1)-3).reshape(self.grid.n,nh1)
             TrQ = Q_fac.solve_A(vtmp)
             TrQc = Q_c_fac.solve_A(vtmp)
-            g_par = np.zeros(37)
-            
-            g_par[36] = self.S.shape[0]*self.r/2 - 1/2*(TrQc*(self.S.transpose()@self.S*np.exp(par[36])@vtmp)).sum()*self.r/nh1 - np.exp(par[36])/2*((data - self.S@mu_c)**2).sum()
-
-            for i in range(9):
-                A_par = Dv@sparse.diags(self.grid.bs[:,i]*np.exp(lkappa))
-                Q_par = A_par.transpose()@iDv@A_mat + A_mat.transpose()@iDv@A_par
-                dQmu_c = Q_par@mu_c
-                g_par[i] =  1/2*((TrQ - TrQc)*(Q_par@vtmp)).sum()*self.r/nh1 - 1/2*(mu_c*(dQmu_c)).sum()
-
-                dH = self.getH(gamma = par[9:18],vx = par[18:27],vy = par[27:36], d=i,grad=True) 
-                for j in range(3):
-                    A_par = - self.Ah(dH[j])
-                    Q_par = A_par.transpose()@iDv@A_mat +  A_mat.transpose()@iDv@A_par
-                    dQmu_c = Q_par@mu_c
-                    g_par[9*(j+1) + i] = 1/2*((TrQ - TrQc)*(Q_par@vtmp)).sum()*self.r/nh1 - 1/2*(mu_c*(dQmu_c)).sum()
+            g_par = np.zeros(par.size)
+            for i in range(par.size-1):
+                dQmu_c = dQ[i]@mu_c
+                g_par[i]= ((1/2*((TrQ - TrQc)*(dQ[i]@vtmp)).sum()*self.r/nh1 - 1/2*(mu_c*(dQmu_c)).sum()))
+            g_par[-1] = self.S.shape[0]*self.r/2 - 1/2*(TrQc*(self.S.T@self.S*tau@vtmp)).sum()*self.r/nh1 - tau/2*((data - self.S@mu_c)**2).sum()
             jac =  -g_par/(self.S.shape[0]*self.r)
-        like =  -like/(self.S.shape[0]*self.r)
-        if grad: 
+            like =  -like/(self.S.shape[0]*self.r)
             return((like,jac))
         else:
+            data  = self.data
+            tau = np.exp(par[-1])
+            Q, Q_fac = self.makeQ(par = par, grad = False)
+            Q_c = Q + self.S.T@self.S*tau
+            Q_c_fac= cholesky(Q_c)
+            mu_c = Q_c_fac.solve_A(self.S.T@data*tau)
+            if self.r == 1:
+                data = data.reshape(-1,1)
+                mu_c = mu_c.reshape(-1,1)
+            like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*np.log(tau)/2 - 1/2*Q_c_fac.logdet()*self.r - 1/2*(mu_c*(Q@mu_c)).sum() - tau/2*((data-self.S@mu_c)**2).sum()
+            like =  -like/(self.S.shape[0]*self.r)
             return(like)
     
-    def Ah(self,Hs) -> sparse.csc_matrix:
+    def Ah(self, Hs) -> sparse.csc_matrix:
         if self.AHnew is None:
             self.setClib()
-        M, N = self.grid.shape
+        M, N, T = self.grid.shape
         Hs = np.array(Hs,dtype = "float64")
         obj = self.AHnew(M, N, Hs, self.grid.hx, self.grid.hy)
         row = self.AHrow(obj)
@@ -172,20 +161,22 @@ class WhittleMaternAnisotropic2D:
         
     def setClib(self) -> None:
         tmp = os.path.dirname(__file__)
-        if not os.path.exists(tmp + '/ccode/lib_AH_2D_b%d.so'%(self.bc)):
-            os.system('g++ -c -fPIC %s/ccode/AH_2D_b%d.cpp -o %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
+            
+        M, N, T = self.grid.shape
+        
+        if not os.path.exists(tmp + '/ccode/lib_AcH_2D_b%d.so'%(self.bc)):
+            os.system('g++ -c -fPIC %s/ccode/AcH_2D_b%d.cpp -o %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
             import platform
             if  'Windows' in platform.system():
-                os.system('g++ -shared -W1, %s/ccode/lib_AH_2D_b%d.so -o %s/ccode/lib_AH_2D_b%d.so %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc)) # not tested
+                os.system('g++ -shared -W1, %s/ccode/lib_AcH_2D_b%d.so -o %s/ccode/lib_AcH_2D_b%d.so %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc)) # not tested
             elif  'Linux' in platform.system():
-                os.system('g++ -shared -W1, %s/ccode/lib_AH_2D_b%d.so -o %s/ccode/lib_AH_2D_b%d.so %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc))
+                os.system('g++ -shared -W1, %s/ccode/lib_AcH_2D_b%d.so -o %s/ccode/lib_AcH_2D_b%d.so %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc))
             else:
-                os.system('g++ -shared -o %s/ccode/lib_AH_2D_b%d.so %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
-            os.system('rm %s/ccode/AH_2D_b%d.o'%(tmp,self.bc))
-        self.libAh = ctypes.cdll.LoadLibrary('%s/ccode/lib_AH_2D_b%d.so'%(tmp,self.bc))
-        M, N = self.grid.shape
+                os.system('g++ -shared -o %s/ccode/lib_AcH_2D_b%d.so %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
+            os.system('rm %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc))
+        self.libAh = ctypes.cdll.LoadLibrary('%s/ccode/lib_AcH_2D_b%d.so'%(tmp,self.bc))
         self.AHnew = self.libAh.AH_new
-        self.AHnew.argtypes = [ctypes.c_int, ctypes.c_int, np.ctypeslib.ndpointer(dtype=np.float64,ndim=4,shape = (M*N,4,2,2)), ctypes.c_double,ctypes.c_double]
+        self.AHnew.argtypes = [ctypes.c_int, ctypes.c_int, np.ctypeslib.ndpointer(dtype=np.float64,ndim=2,shape = (2,2)), ctypes.c_double,ctypes.c_double]
         self.AHnew.restype = ctypes.c_void_p
         self.AHrow = self.libAh.AH_Row
         self.AHrow.argtypes = [ctypes.c_void_p]
--- src/spdepy/spdes/whittle_matern_ha2D.py
+++ src/spdepy/spdes/whittle_matern_ha2D.py
@@ -17,7 +17,7 @@ class WhittleMaternHa2D:
         self.AHnew = None
         self.Awnew = None
         if par is None: 
-            par = np.hstack([[-1]*9,[-1]*9, [0.1]*9, [0.1]*9,1],dtype="float64")
+            par = np.array([-1,-1,0.1,0.1,9,0],dtype="float64")
             self.setPars(par)
         else:
             self.setQ(par = par)
@@ -27,11 +27,11 @@ class WhittleMaternHa2D:
     
     def setPars(self,par) -> None:
         par = np.array(par,dtype="float64")
-        self.kappa = par[0:9]
-        self.gamma = par[9:18]
-        self.vx = par[18:27]
-        self.vy = par[27:36]
-        self.tau = par[36]
+        self.kappa = par[0]
+        self.gamma = par[1]
+        self.vx = par[2]
+        self.vy = par[3]
+        self.tau = par[4]
         self.sigma = np.log(np.sqrt(1/np.exp(self.tau)))
         
     def transDiff(self,par = None):
@@ -45,9 +45,8 @@ class WhittleMaternHa2D:
         self.tvy = np.sqrt(np.exp(par[0])*sinh_aV/aV*(-par[2]+ aV))
 
     def initFit(self,data, **kwargs):
-        #mod4: kappa(0:9), gamma(9:18), vx(18:27), vy(27:36), sigma(36)
         assert data.shape[0] <= self.grid.Ns
-        par = np.hstack([[-1]*9,[-1]*9, [0.1]*9, [0.1]*9,1],dtype="float64")
+        par = np.array([-1,-1,0.1,0.1,9,0],dtype="float64")
         self.data = data
         if self.data.ndim == 2:
             self.r = self.data.shape[1]
@@ -56,127 +55,107 @@ class WhittleMaternHa2D:
         self.S = self.grid.getS()
         return(par)
 
-    def sample(self,n = 1,simple = False):
-        z = np.random.normal(size = self.grid.Ns*n).reshape(self.grid.Ns,n)
-        data = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False))
-        if not simple:
-            data += z*np.exp(self.sigma)
-        return(data)
 
-
-    def setQ(self,par = None,S = None, simple = False):
+    def setQ(self,par = None,S = None):
         if par is None:
-            assert(self.kappa is not None and self.vx is not None and self.vy is not None and self.gamma is not None and self.sigma is not None)
             par = self.getPars()
         else:
             self.setPars(par)
-        if S is not None:
-            self.S = S
-        Hs = self.getH()
-        Dk =  sparse.diags(np.exp(self.grid.evalB(par = self.kappa))) 
-        A_mat = self.grid.Dv@Dk - self.Ah(Hs)
-        self.Q = A_mat.T@self.grid.iDv@A_mat
+        self.Q, self.Q_fac = self.makeQ(par = par, grad = False)
         self.S = self.grid.getS()
-        if not simple:
-            self.Q_fac = cholesky(self.Q)
 
-    def getH(self,gamma=None,vx = None,vy = None,d=None,grad = False):
-        if vx is None and vy is None and gamma is None:
-            gamma = self.gamma
-            vx = self.vx
-            vy = self.vy
-        if not grad:
-            pg = np.exp(self.grid.evalBH(par = gamma))
-            pvx = self.grid.evalBH(par = vx)
-            pvy = self.grid.evalBH(par = vy)
-            aV = np.sqrt(pvx**2 + pvy**2)
-            mV = np.array([[pvx,pvy],[pvy,-pvx]]).T.swapaxes(0,1)
-            cosh_aV = (np.exp(aV)+ np.exp(-aV))/2
-            sinh_aV = (np.exp(aV)- np.exp(-aV))/2
-            H = (pg*cosh_aV)[:,:,np.newaxis,np.newaxis]*np.eye(2) + (pg*sinh_aV/aV)[:,:,np.newaxis,np.newaxis]*mV
-            return(H)
-        else:
-            dpar = np.zeros(9)
-            dpar[d] = 1
-
-            pg = np.exp(self.grid.evalBH(par = gamma))
-            pvx = self.grid.evalBH(par = vx)
-            pvy = self.grid.evalBH(par = vy)
-            aV = np.sqrt(pvx**2 + pvy**2)
-            mV = np.array([[pvx,pvy],[pvy,-pvx]]).T.swapaxes(0,1)
-            cosh_aV = (np.exp(aV)+ np.exp(-aV))/2
-            sinh_aV = (np.exp(aV)- np.exp(-aV))/2
-            
-            dpg = self.grid.bsH[:,:,d]*pg
-            H_gamma = (dpg*cosh_aV)[:,:,np.newaxis,np.newaxis]*np.eye(2) + (dpg*sinh_aV/aV)[:,:,np.newaxis,np.newaxis]*mV
-
-            dpvx = self.grid.evalBH(par = dpar)
-            dmV = np.array([[dpvx,pvy*0],[pvy*0,-dpvx]]).T.swapaxes(0,1)
-            H_vx = (pg*sinh_aV*dpvx*pvx/aV)[:,:,np.newaxis,np.newaxis]*np.eye(2) + (pg*(cosh_aV*pvx*dpvx - sinh_aV*pvx*dpvx/aV)/aV**2)[:,:,np.newaxis,np.newaxis]*mV + (pg*sinh_aV/aV)[:,:,np.newaxis,np.newaxis]*dmV
-            
-
-            dpvy = self.grid.evalBH(par = dpar)
-            dmV = np.array([[pvx*0,dpvy],[dpvy,-pvx*0]]).T.swapaxes(0,1)
-            H_vy = (pg*sinh_aV*dpvy*pvy/aV)[:,:,np.newaxis,np.newaxis]*np.eye(2) + (pg*(cosh_aV*pvy*dpvy - sinh_aV*pvy*dpvy/aV)/aV**2)[:,:,np.newaxis,np.newaxis]*mV + (pg*sinh_aV/aV)[:,:,np.newaxis,np.newaxis]*dmV
-            return((H_gamma,H_vx,H_vy))
-
-    def print(self,par):
-        return("| \u03BA = %2.2f"%(np.exp(par[0:9]).mean()) +  ", \u03B3 = %2.2f"%(np.exp(par[9:18]).mean()) + ", vx = %2.2f"%(par[18:27].mean()) + ", vy = %2.2f"%(par[27:36].mean()) +  ",\u03C4 = %2.2f"%(np.exp(par[36])))
     
-    def logLike(self, par, nh1 = 100,grad = True):
-        #mod4: kappa(0:9), gamma(9:18), vx(18:27), vy(27:36), sigma(36)
-        data  = self.data
-        Hs = self.getH(gamma = par[9:18],vx = par[18:27], vy = par[27:36]) 
-        lkappa = self.grid.evalB(par = par[0:9])
-        Dk =  sparse.diags(np.exp(lkappa))
+    def makeQ(self, par, grad = True):
+        assert self.Q0 is not None
+        Ns = self.grid.Ns
         Dv = self.grid.Dv
         iDv = self.grid.iDv
-        A_mat = Dv@Dk - self.Ah(Hs)
-        Q = A_mat.transpose()@iDv@A_mat
-        Q_c = Q + self.S.transpose()@self.S*np.exp(par[36])
+        # parameters
+        kappa = np.exp(par[0])
+        gamma = np.exp(par[1])
+        vx = par[2]
+        vy = par[3]
+        aV = np.sqrt(vx**2 + vy**2)
+        mV = np.array([[vx,vy],[vy,-vx]])
+        cosh_aV = (np.exp(aV) + np.exp(-aV))/2
+        sinh_aV = (np.exp(aV) - np.exp(-aV))/2
+        # components
+        Hs = gamma*(cosh_aV*np.eye(2) + sinh_aV/aV*mV)
+        Dk =  kappa*sparse.eye(Ns) 
+        A_H = self.Ah(Hs)
+        A =  Dv@Dk - A_H
+        Q = A.transpose()@iDv@A
+        # gradient
         Q_fac = cholesky(Q)
-        Q_c_fac= cholesky(Q_c)
-        if (Q_fac == -1) or (Q_c_fac == -1):
-            if grad:
-                return((self.like,self.jac))
-            else:
-                return(self.like)
-        mu_c = Q_c_fac.solve_A(self.S.transpose()@data*np.exp(par[36]))
-        if self.r == 1:
-            data = data.reshape(-1,1)
-            mu_c = mu_c.reshape(-1,1)
-        like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*par[36]/2 - 1/2*Q_c_fac.logdet()*self.r - 1/2*(mu_c*(Q@mu_c)).sum() - np.exp(par[36])/2*((data-self.S@mu_c)**2).sum()
         if grad:
-            vtmp = (2*np.random.randint(1,3,self.grid.Ns*nh1)-3).reshape(self.grid.Ns,nh1)
+            dQ = []
+            # log kappa 2
+            dA = (Dv@Dk).tocsc()
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            # log gamma
+            dA = - A_H
+            A =  Dv@Dk - self.Ah(Hs)
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            # vx 
+            dHs = gamma/aV*(vx*sinh_aV*np.eye(2) + vx/aV*(cosh_aV - sinh_aV/aV)*mV + sinh_aV*np.array([[1,0],[0,-1]]))
+            dA = - self.Ah(dHs)
+            A =  Dv@Dk - self.Ah(Hs)
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            # vy
+            dHs = gamma/aV*(vy*sinh_aV*np.eye(2) + vy/aV*(cosh_aV - sinh_aV/aV)*mV + sinh_aV*np.array([[0,1],[1,0]]))
+            dA = - self.Ah(dHs)
+            A =  Dv@Dk - self.Ah(Hs)
+            dQ.append(dA.T@iDv@A + A.T@iDv@dA)
+            return(Q, Q_fac, dQ)
+        else:
+            return(Q, Q_fac)
+
+    def print(self,par):
+        return("| \u03BA = %2.2f"%(np.exp(par[0])) +  ", \u03B3 = %2.2f"%(np.exp(par[1])) + ", vx = %2.2f"%(par[2]) + ", vy = %2.2f"%(par[3]) +  ",\u03C4 = %2.2f"%(np.exp(par[4])))
+    
+
+    def logLike(self, par, nh1 = 100, grad = True):
+        if grad:
+            data  = self.data
+            tau = np.exp(par[-1])
+            Q, Q_fac, dQ = self.makeQ(par = par, grad = True)
+            Q_c = Q + self.S.T@self.S*tau
+            Q_c_fac = cholesky(Q_c)
+            mu_c = Q_c_fac.solve_A(self.S.T@data*tau)
+            if self.r == 1:
+                data = data.reshape(-1,1)
+                mu_c = mu_c.reshape(-1,1)
+            like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*np.log(tau)/2 - 1/2*Q_c_fac.logdet()*self.r - 1/2*(mu_c*(Q@mu_c)).sum() - tau/2*((data-self.S@mu_c)**2).sum()
+            
+            vtmp = (2*np.random.randint(1,3,self.grid.n*nh1)-3).reshape(self.grid.n,nh1)
             TrQ = Q_fac.solve_A(vtmp)
             TrQc = Q_c_fac.solve_A(vtmp)
-            g_par = np.zeros(37)
-            
-            g_par[36] = self.S.shape[0]*self.r/2 - 1/2*(TrQc*(self.S.transpose()@self.S*np.exp(par[36])@vtmp)).sum()*self.r/nh1 - np.exp(par[36])/2*((data - self.S@mu_c)**2).sum()
-
-            for i in range(9):
-                A_par = Dv@sparse.diags(self.grid.bs[:,i]*np.exp(lkappa))
-                Q_par = A_par.transpose()@iDv@A_mat + A_mat.transpose()@iDv@A_par
-                dQmu_c = Q_par@mu_c
-                g_par[i] =  1/2*((TrQ - TrQc)*(Q_par@vtmp)).sum()*self.r/nh1 - 1/2*(mu_c*(dQmu_c)).sum()
-
-                dH = self.getH(gamma = par[9:18],vx = par[18:27],vy = par[27:36], d=i,grad=True) 
-                for j in range(3):
-                    A_par = - self.Ah(dH[j])
-                    Q_par = A_par.transpose()@iDv@A_mat +  A_mat.transpose()@iDv@A_par
-                    dQmu_c = Q_par@mu_c
-                    g_par[9*(j+1) + i] = 1/2*((TrQ - TrQc)*(Q_par@vtmp)).sum()*self.r/nh1 - 1/2*(mu_c*(dQmu_c)).sum()
+            g_par = np.zeros(par.size)
+            for i in range(par.size-1):
+                dQmu_c = dQ[i]@mu_c
+                g_par[i]= ((1/2*((TrQ - TrQc)*(dQ[i]@vtmp)).sum()*self.r/nh1 - 1/2*(mu_c*(dQmu_c)).sum()))
+            g_par[-1] = self.S.shape[0]*self.r/2 - 1/2*(TrQc*(self.S.T@self.S*tau@vtmp)).sum()*self.r/nh1 - tau/2*((data - self.S@mu_c)**2).sum()
             jac =  -g_par/(self.S.shape[0]*self.r)
-        like =  -like/(self.S.shape[0]*self.r)
-        if grad: 
+            like =  -like/(self.S.shape[0]*self.r)
             return((like,jac))
         else:
+            data  = self.data
+            tau = np.exp(par[-1])
+            Q, Q_fac = self.makeQ(par = par, grad = False)
+            Q_c = Q + self.S.T@self.S*tau
+            Q_c_fac= cholesky(Q_c)
+            mu_c = Q_c_fac.solve_A(self.S.T@data*tau)
+            if self.r == 1:
+                data = data.reshape(-1,1)
+                mu_c = mu_c.reshape(-1,1)
+            like = 1/2*Q_fac.logdet()*self.r + self.S.shape[0]*self.r*np.log(tau)/2 - 1/2*Q_c_fac.logdet()*self.r - 1/2*(mu_c*(Q@mu_c)).sum() - tau/2*((data-self.S@mu_c)**2).sum()
+            like =  -like/(self.S.shape[0]*self.r)
             return(like)
-
-    def Ah(self,Hs) -> sparse.csc_matrix:
+    
+    def Ah(self, Hs) -> sparse.csc_matrix:
         if self.AHnew is None:
             self.setClib()
-        M, N = self.grid.shape
+        M, N, T = self.grid.shape
         Hs = np.array(Hs,dtype = "float64")
         obj = self.AHnew(M, N, Hs, self.grid.hx, self.grid.hy)
         row = self.AHrow(obj)
@@ -192,31 +171,33 @@ class WhittleMaternHa2D:
         return(res)
         
     def setClib(self) -> None:
-        M, N = self.grid.shape
         tmp = os.path.dirname(__file__)
-        if not os.path.exists(tmp + '/ccode/lib_AH_2D_b%d.so'%(self.bc)):
-            os.system('g++ -c -fPIC %s/ccode/AH_2D_b%d.cpp -o %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
+            
+        M, N, T = self.grid.shape
+        
+        if not os.path.exists(tmp + '/ccode/lib_AcH_2D_b%d.so'%(self.bc)):
+            os.system('g++ -c -fPIC %s/ccode/AcH_2D_b%d.cpp -o %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
             import platform
             if  'Windows' in platform.system():
-                os.system('g++ -shared -W1, %s/ccode/lib_AH_2D_b%d.so -o %s/ccode/lib_AH_2D_b%d.so %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc)) # not tested
+                os.system('g++ -shared -W1, %s/ccode/lib_AcH_2D_b%d.so -o %s/ccode/lib_AcH_2D_b%d.so %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc)) # not tested
             elif  'Linux' in platform.system():
-                os.system('g++ -shared -W1, %s/ccode/lib_AH_2D_b%d.so -o %s/ccode/lib_AH_2D_b%d.so %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc))
+                os.system('g++ -shared -W1, %s/ccode/lib_AcH_2D_b%d.so -o %s/ccode/lib_AcH_2D_b%d.so %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc,tmp,self.bc))
             else:
-                os.system('g++ -shared -o %s/ccode/lib_AH_2D_b%d.so %s/ccode/AH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
-            os.system('rm %s/ccode/AH_2D_b%d.o'%(tmp,self.bc))
-        self.lib = ctypes.cdll.LoadLibrary('%s/ccode/lib_AH_2D_b%d.so'%(tmp,self.bc))
-        self.AHnew = self.lib.AH_new
-        self.AHnew.argtypes = [ctypes.c_int, ctypes.c_int, np.ctypeslib.ndpointer(dtype=np.float64,ndim=4,shape = (M*N,4,2,2)), ctypes.c_double,ctypes.c_double]
+                os.system('g++ -shared -o %s/ccode/lib_AcH_2D_b%d.so %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc,tmp,self.bc))
+            os.system('rm %s/ccode/AcH_2D_b%d.o'%(tmp,self.bc))
+        self.libAh = ctypes.cdll.LoadLibrary('%s/ccode/lib_AcH_2D_b%d.so'%(tmp,self.bc))
+        self.AHnew = self.libAh.AH_new
+        self.AHnew.argtypes = [ctypes.c_int, ctypes.c_int, np.ctypeslib.ndpointer(dtype=np.float64,ndim=2,shape = (2,2)), ctypes.c_double,ctypes.c_double]
         self.AHnew.restype = ctypes.c_void_p
-        self.AHrow = self.lib.AH_Row
+        self.AHrow = self.libAh.AH_Row
         self.AHrow.argtypes = [ctypes.c_void_p]
         self.AHrow.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_int, shape = (M*N*9,))
-        self.AHcol = self.lib.AH_Col
+        self.AHcol = self.libAh.AH_Col
         self.AHcol.argtypes = [ctypes.c_void_p]
         self.AHcol.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_int, shape = (M*N*9,))
-        self.AHval = self.lib.AH_Val
+        self.AHval = self.libAh.AH_Val
         self.AHval.argtypes = [ctypes.c_void_p]
         self.AHval.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_double, shape = (M*N*9,))
-        self.AHdel = self.lib.AH_delete
+        self.AHdel = self.libAh.AH_delete
         self.AHdel.argtypes = [ctypes.c_void_p]
         self.AHdel.restype = None
